759818841c2ea55660ddb76d19f57fd6e27e2be9
BrandonPoll <Brandon.r.poll@gmail>
2019-02-04 16:51:33 -0500

    Added Project Files

---
+++ Vivarium/Intermediate/Build/Win64/UE4Editor/Inc/Vivarium/VivariumGameModeBase.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef VIVARIUM_VivariumGameModeBase_generated_h
+#error "VivariumGameModeBase.generated.h already included, missing '#pragma once' in VivariumGameModeBase.h"
+#endif
+#define VIVARIUM_VivariumGameModeBase_generated_h
+
+#define Vivarium_Source_Vivarium_VivariumGameModeBase_h_15_RPC_WRAPPERS
+#define Vivarium_Source_Vivarium_VivariumGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Vivarium_Source_Vivarium_VivariumGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAVivariumGameModeBase(); \
+	friend struct Z_Construct_UClass_AVivariumGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AVivariumGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/Vivarium"), NO_API) \
+	DECLARE_SERIALIZER(AVivariumGameModeBase)
+
+
+#define Vivarium_Source_Vivarium_VivariumGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAVivariumGameModeBase(); \
+	friend struct Z_Construct_UClass_AVivariumGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AVivariumGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/Vivarium"), NO_API) \
+	DECLARE_SERIALIZER(AVivariumGameModeBase)
+
+
+#define Vivarium_Source_Vivarium_VivariumGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AVivariumGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AVivariumGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVivariumGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVivariumGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVivariumGameModeBase(AVivariumGameModeBase&&); \
+	NO_API AVivariumGameModeBase(const AVivariumGameModeBase&); \
+public:
+
+
+#define Vivarium_Source_Vivarium_VivariumGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AVivariumGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVivariumGameModeBase(AVivariumGameModeBase&&); \
+	NO_API AVivariumGameModeBase(const AVivariumGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVivariumGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVivariumGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AVivariumGameModeBase)
+
+
+#define Vivarium_Source_Vivarium_VivariumGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define Vivarium_Source_Vivarium_VivariumGameModeBase_h_12_PROLOG
+#define Vivarium_Source_Vivarium_VivariumGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Vivarium_Source_Vivarium_VivariumGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Vivarium_Source_Vivarium_VivariumGameModeBase_h_15_RPC_WRAPPERS \
+	Vivarium_Source_Vivarium_VivariumGameModeBase_h_15_INCLASS \
+	Vivarium_Source_Vivarium_VivariumGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Vivarium_Source_Vivarium_VivariumGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Vivarium_Source_Vivarium_VivariumGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Vivarium_Source_Vivarium_VivariumGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Vivarium_Source_Vivarium_VivariumGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	Vivarium_Source_Vivarium_VivariumGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Vivarium_Source_Vivarium_VivariumGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Vivarium/Intermediate/Build/Win64/VivariumEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "D:/The Projects/Unreal Repos/Vivarium/Vivarium/Intermediate/Build/Win64/VivariumEditor/Development/Engine/SharedPCH.Engine.h"
